1c1f69f03a61401f386f4c6b077ad66dfa754f2c
chuang-shenmi <jlhclc1107@gmail.com>
2026-08-27 20:45:21 -0700

    Translate comments and error messages to English

--- finite lens/MatrixBalanceJacobi.m
+++ finite lens/MatrixBalanceJacobi.m
@@ -7,23 +7,23 @@ function [x]=MatrixBalanceJacobi(A,b)
 
 [m,n] = size(A);
 if m ~= n
-    error("A不是一个方阵!")
+    error("A is not square!")
 end
 m = length(b);
 if m ~= n
-    error("A和b的维度不一致，无法形成方程")
+    error("The dimensions of A and b do not match, so an equation cannot be formed.")
 end
 
-% 构建对角缩放（Diagonal Scaling）法所需对角矩阵
+% Constructing the diagonal matrix required for the diagonal scaling method
 D = zeros(n,n);
 for i = 1:n
-    D(i,i) = 1/sqrt(max(abs(A(i,i)),realmin));  % 为防止A对角线元素过小出现除以0的情况，取机器精度和对角元中最大值
+    D(i,i) = 1/sqrt(max(abs(A(i,i)),realmin)); % To prevent division by 0 due to the diagonal elements of A being too small, use machine precision and the maximum value among the diagonal elements
 end
 
-% 建立并求解等价方程 A_hat*x_hat=b_hat
+% Formulate and solve the equivalent equation A_hat*x_hat = b_hat
 A_hat = D*A*D;
 b_hat = D*b;
 x_hat = A_hat \ b_hat;
 
-% 将解还原回x
+% Express the solution in terms of x.
 x = D*x_hat;
